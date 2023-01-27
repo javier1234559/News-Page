547c5b5a1b2f54f689e775c00c01cc308414b8de
nhat1234559 <nhat1234559@gmail.com>
2023-01-27 16:35:37 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-red-400">
+        hello
+    </div>
+  )
+}
+
+export default App
